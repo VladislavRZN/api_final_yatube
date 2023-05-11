5e782eb43bb3444ae5a2c898590463da6c44e336
Владислав Буганов <vladislav-buganov@yandex.ru>
2023-05-12 02:53:47 +0300

    Исправление flake8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -17,7 +17,7 @@ class GroupSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        slug_field='username', 
+        slug_field='username',
         read_only=True,
     )
 
