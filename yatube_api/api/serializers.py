f82b2690207991930cfafa41b334d8c844caca1b
Владислав Буганов <vladislav-buganov@yandex.ru>
2023-05-11 23:33:51 +0300

    Первая попытка

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,25 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
+from posts.models import Comment, Follow, Group, Post, User
 
-from posts.models import Comment, Post
+FOLLOW_ERROR_MESSAGE_USER_IS_FOLLOWER = ('Пользователь уже '
+                                         'подписан на этого автора')
+FOLLOW_ERROR_MESSAGE_USER_IS_AUTHOR = ('Пользователь не может '
+                                       'подписаться на самого себя')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username', 
+        read_only=True,
+    )
 
     class Meta:
         fields = '__all__'
@@ -15,9 +28,41 @@ class PostSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True,
+        slug_field='username',
     )
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = (
+            SlugRelatedField(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=FOLLOW_ERROR_MESSAGE_USER_IS_FOLLOWER
+            ),
+        )
+
+    def validate_following(self, value):
+        if self.context['request'].user == value:
+            raise serializers.ValidationError(
+                FOLLOW_ERROR_MESSAGE_USER_IS_AUTHOR
+            )
+        return value
--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Follow, Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "author", "group")
+    list_editable = ("group",)
+    search_fields = ("text",)
+    empty_value_display = "-пусто-"
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'slug',
+        'title',
+        'description',
+    )
+    search_fields = ('title', 'description',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'post',
+        'author',
+        'text',
+        'created'
+    )
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'following'
+    )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
