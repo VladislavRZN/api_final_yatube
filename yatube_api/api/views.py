23366c293355b85b85d7d918367f0e2e9fd7b371
Владислав Буганов <vladislav-buganov@yandex.ru>
2023-05-12 23:48:54 +0300

    ИСправлено после ревью

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import filters, mixins, pagination, viewsets
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 
 from posts.models import Group, Post
 from .permissions import IsAuthorOrReadOnly
